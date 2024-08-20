32abc7034ea709b42a0170f5de760712270791c4
jauhar-imtikhan <bangjoe.muns@gmail.com>
2024-08-20 14:01:15 +0700

    update modules [Admin & Frontend]

---
+++ Modules/Admin/app/Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Modules\Admin\Providers\Filament;
+
+use Coolsam\Modules\Concerns\ModuleFilamentPlugin;
+use Coolsam\Modules\ModulesPlugin;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class AdminPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->path('panel')
+            ->plugin(ModulesPlugin::make())
+            ->login()
+            ->colors([
+                'primary' => Color::Emerald,
+            ])
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'Modules\\Admin\\App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'Modules\\Admin\\App\\Filament\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'Moduls\\Admin\\App\\Filament\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
